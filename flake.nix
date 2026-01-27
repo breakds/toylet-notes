032e8100493645d53814e3086d5323ba8dcde6eb
Break Yang <breakds@gmail.com>
2026-01-27 08:56:47 -0800

    Add reading-desk to devshell

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Break's notes collection for reading-desk";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
+    reading-desk.url = "github:breakds/reading-desk";
+    reading-desk.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { flake-parts, ... }@inputs: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" "aarch64-darwin" ];
+
+    perSystem = { system, pkgs, ... }: {
+      _module.args.pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [ inputs.reading-desk.overlays.default ];
+      };
+
+      devShells.default = pkgs.mkShell {
+        name = "toylet-notes";
+        packages = [ pkgs.reading-desk ];
+      };
+    };
+  };
+}
